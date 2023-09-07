312c24d447b414bdecc5be76bcd113055c01e84c
mercurial <d.nespayev@gekata.io>
2023-09-08 00:55:01 +0600

    feat: add `Message` model

--- api/src/telegram_bot/models.py
+++ api/src/telegram_bot/models.py
@@ -1,4 +1,5 @@
 import uuid
+from datetime import datetime
 
 from django.contrib.auth import get_user_model
 from django.db import models
@@ -8,3 +9,10 @@ class UserBotToken(models.Model):
     token = models.UUIDField(default=uuid.uuid4)
     user = models.OneToOneField(to=get_user_model(), on_delete=models.CASCADE)
     chat_id = models.BigIntegerField(blank=True, null=True)
+
+
+class Message(models.Model):
+    user = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE, db_index=True)
+    chat_id = models.BigIntegerField(db_index=True)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
